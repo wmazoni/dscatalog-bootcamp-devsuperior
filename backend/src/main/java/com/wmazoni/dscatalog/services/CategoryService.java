e27417188044c6db3b165a31c7d3cbfeb9d4854d
Wellington Andrade <wmazoni@protonmail.com>
2020-10-03 09:12:37 -0300

    Insert Category

--- backend/src/main/java/com/wmazoni/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/wmazoni/dscatalog/services/CategoryService.java
@@ -21,13 +21,21 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
         List<Category> list = categoryRepository.findAll();
-        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+        return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = categoryRepository.findById(id);
         Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = categoryRepository.save(entity);
         return new CategoryDTO(entity);
     }
 }
