fee1db219395ec91a9e653f2e6c3189298847c6f
Wellington Andrade <wmazoni@protonmail>
2020-11-09 17:37:09 -0300

    Add base admin from structure

---
+++ front-web/src/pages/Admin/components/Navbar/Products/Form/index.tsx
@@ -0,0 +1,17 @@
+import BaseForm from 'pages/Admin/components/BaseForm';
+import React from 'react';
+import './styles.scss';
+
+const Form = () => {
+    return (
+        <BaseForm title="cadastrar um produto" >
+            <div className="row">
+                <div className="col-6">
+                    <input type="text" className="form-control"/>
+                </div>
+            </div>
+        </BaseForm>
+    )
+}
+
+export default Form;
