a017cc52589aaaab09eff44f860310226c3fcdb5
Wellington Andrade <wmazoni@protonmail.com>
2021-03-12 11:14:22 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/wmazoni/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/wmazoni/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.wmazoni.dscatalog.dto;
 
+import com.wmazoni.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
     private static final long serialVersionUID = 1L;
