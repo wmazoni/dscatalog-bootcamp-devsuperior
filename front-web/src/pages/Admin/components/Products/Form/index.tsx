4dac7ab8f4897d5993f6099fdf5774898f578a81
Wellington Mazoni <wmazoni@protonmail.com>
2021-09-02 08:59:07 -0300

    front-end: Validation ProductName form with React Hook Form

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -12,10 +12,11 @@ type FormState = {
 }
 
 const Form = () => {
-    const { register, handleSubmit } = useForm<FormState>();
+    const { register, handleSubmit, formState: { errors } } = useForm<FormState>();
 
     const onSubmit = (data: FormState) => {
-        makePrivateRequest({ url: '/products', method: 'POST', data });
+        console.log(data)
+        //makePrivateRequest({ url: '/products', method: 'POST', data });
     }
 
     return (
@@ -23,27 +24,51 @@ const Form = () => {
             <BaseForm title="cadastrar um produto" >
                 <div className="row">
                     <div className="col-6">
-                        <input
-                            {...register('name', { required: "Campo obrigatório" })}
-                            name="name"
-                            type="text"
-                            className="form-control margin-bottom-30 input-base"
-                            placeholder="Nome do Produto"
-                        />
-                        <input
-                            {...register('price', { required: "Campo obrigatório" })}
-                            name="price"
-                            type="number"
-                            className="form-control margin-bottom-30 input-base"
-                            placeholder="Preço"
-                        />
-                        <input
-                            {...register('imageUrl', { required: "Campo obrigatório" })}
-                            name="imageUrl"
-                            type="text"
-                            className="form-control margin-bottom-30 input-base"
-                            placeholder="Imagem do Produto"
-                        />
+                        <div className="margin-bottom-30">
+                            <input
+                                {...register('name', { 
+                                required: "Campo obrigatório",
+                                minLength: {value: 5, message: 'O campo deve ter no mínimo 5 caracteres'},
+                                maxLength: {value: 60, message: 'O campo deve ter no máximo 60 caracteres'} })}
+                                name="name"
+                                type="text"
+                                className="form-control input-base"
+                                placeholder="Nome do Produto"
+                            />
+                            {errors.name && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.name.message}
+                                </div>
+                            )}
+                        </div>
+                        <div className="margin-bottom-30">
+                            <input
+                                {...register('price', { required: "Campo obrigatório" })}
+                                name="price"
+                                type="number"
+                                className="form-control input-base"
+                                placeholder="Preço"
+                            />
+                            {errors.price && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.price.message}
+                                </div>
+                            )}
+                        </div>
+                        <div className="margin-bottom-30">
+                            <input
+                                {...register('imageUrl', { required: "Campo obrigatório" })}
+                                name="imageUrl"
+                                type="text"
+                                className="form-control input-base"
+                                placeholder="Imagem do Produto"
+                            />
+                            {errors.imageUrl && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.imageUrl.message}
+                                </div>
+                            )}
+                        </div>
                     </div>
                     <div className="col-6">
                         <textarea
@@ -54,6 +79,11 @@ const Form = () => {
                             cols={30}
                             rows={10}
                         />
+                        {errors.description && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.description.message}
+                                </div>
+                            )}
                     </div>
                 </div>
             </BaseForm>
